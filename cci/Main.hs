12dff068ad3445770a1cb853cdb1bd3fc831fc60
Christopher Vogt <oss.nsp@cvogt.org>
2019-05-06 12:35:39 -0400

    template for training session

--- cci/Main.hs
+++ cci/Main.hs
@@ -1,17 +1,9 @@
 module Main where
 
-import qualified Circleci
+--import qualified Circleci
 
 import           Protolude
-import           System.Directory (getCurrentDirectory)
 
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    ["list-here"] -> Circleci.list =<< getCurrentDirectory
-    ["list-in",repo] -> Circleci.list repo
-    ["trigger-here"] -> Circleci.trigger =<< getCurrentDirectory
-    ["trigger-in", repo] -> Circleci.trigger repo
-    ["list-previous"] -> Circleci.listResults
-    _ -> putStrLn ("invalid usage" :: Text)
+  putStrLn ("Hello World" :: Text)
